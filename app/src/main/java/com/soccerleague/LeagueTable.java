d2ce6c3735d459c8e3e9d4e6e88d14ff1d592fd9
Ajit Beeki <abeeki20@bu.edu>
2018-12-25 22:13:18 -0600

    Initial commit

---
+++ app/src/main/java/com/soccerleague/LeagueTable.java
@@ -0,0 +1,22 @@
+package com.soccerleague;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class LeagueTable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_league_table);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
